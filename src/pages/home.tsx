5f948506c0add8d0f4895e58004b7499f6b40866
Umut Özdoğan <umut@otsimo.com>
2021-09-09 00:02:42 +0300

    home: add initial page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 // material-ui
-import { Theme, makeStyles, createStyles } from "@material-ui/core";
+import { Theme, makeStyles, createStyles, Button } from "@material-ui/core";
 
 // exports
 export { Home };
@@ -9,7 +10,38 @@ export { Home };
 interface HomeProps {}
 
 function Home(props: HomeProps) {
-  return <>Home Page</>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <RouterLink to="/menu">
+        <Button className={classes.button} variant="contained" color="primary">
+          Menü
+        </Button>
+      </RouterLink>
+      <RouterLink to="/login">
+        <Button className={classes.button} variant="contained" color="primary">
+          Giriş yap
+        </Button>
+      </RouterLink>
+    </div>
+  );
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
+    },
+    button: {
+      margin: 30,
+      borderRadius: 48,
+      width: 250,
+      height: 70,
+      fontSize: 30,
+    },
+  })
+);
